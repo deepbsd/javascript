f3f1e2b9cacdc48270f6f25db0503e3754080282
David S. Jackson <deepbsd@yahoo.com>
2017-02-01 16:50:19 -0500

    updates to shopping list app

---
+++ projects/tf-shoppinglist2/app.js
@@ -0,0 +1,28 @@
+
+
+// Single state object
+var state = {
+    items: []
+};
+
+// State modification functions
+var addItem = function(state, item) {
+    state.items.push(item);
+    //console.log(items);
+};
+
+// Render functions
+var renderList = function(state, element) {
+    var itemsHTML = state.items.map(function(item) {
+        return '<li>' + item + '</li>';
+    });
+    element.html(itemsHTML);
+};
+
+// Event listeners
+$('.js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    addItem(state, $('.shopping-list-entry').val());
+    renderList(state, $('.shopping-list'));
+});
+
