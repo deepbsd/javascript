e7d93c302eae013968f9becfecf24fee1255d4ba
David S. Jackson <deepbsd@yahoo.com>
2017-02-01 12:35:25 -0500

    first time with tf-shoppinglist app

---
+++ projects/jqTextAnalyzer/app2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*  Keep stuff separated from actual main function...  Trying a new organization  */
+
+// set up some variables that contain calls to functions
+
+// set up functions used by variables described above
+
+$( function textAnalyzer() {
+
+});
