d4db0713ad28fffcdd6d23ed0d14aa5cd16435c0
David S. Jackson <deepbsd@yahoo.com>
2017-02-01 11:57:33 -0500

    worked on fizzbuzz and textanalyzer

--- projects/jqFizzBuzz/script.js
+++ projects/jqFizzBuzz/script.js
@@ -1,11 +1,18 @@
 
 $( function fizzBuzz() {
+
+
    
-   $('#number-chooser').submit( function(event) {
+   $('#number-chooser').submit( function(ev) {
+
+
+
      var numberIn = parseInt($('#number-choice').val());
       
+      ev.preventDefault(); 
+
      if (!numberIn.isNaN) {
-      
+
        for (var i=1; i <= numberIn; i++){
          if (i%3 === 0 && i%5 === 0){
            $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>FizzBuzz</span></p>');
@@ -17,6 +24,7 @@ $( function fizzBuzz() {
            $('.js-results').append('<div class="fizz-buzz-item"><span>'+i+'</span></p>');
          }  // end of if stmt
        }  // end of for loop
+
      }  // end of if !numberIn.isNaN
      
      
@@ -24,3 +32,17 @@ $( function fizzBuzz() {
 
 
 });
+
+/*     Looking at a different idea here...
+$('#my-form-element').submit(function (ev) {
+  // ev is an abbreviation of event - which is a reserved word in JavaScript
+  // and should not be used as variable name
+
+  // it prevents the default browser behavior - which redirects you to a
+  // new page
+  ev.preventDefault()
+
+  var form = $(ev.target)
+  // here you do some work with your form element
+})
+*/
