ebfbadad0390e8f04e852fb54dbb18ae639ead2c
David S. Jackson <deepbsd@yahoo.com>
2017-02-02 11:22:40 -0500

    working on shopplinglist challenge in projects/shoppinglist2

--- projects/tf-shoppinglist2/app.js
+++ projects/tf-shoppinglist2/app.js
@@ -2,27 +2,31 @@
 
 // Single state object
 var state = {
-    items: []
+    items: ["apples", "oranges", "milk", "bread"]
 };
 
 // State modification functions
 var addItem = function(state, item) {
     state.items.push(item);
-    //console.log(items);
+    console.log('addItem: '+item);
 };
 
 // Render functions
 var renderList = function(state, element) {
+	console.log(element);
+	var startContainer = '<li> <span class="shopping-item">';
+	var endContainer = '</span> <div class="shopping-item-controls"><button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>';
     var itemsHTML = state.items.map(function(item) {
-        return '<li>' + item + '</li>';
+    	console.log('renderList: '+startContainer+item+endContainer);
+        return startContainer + item + endContainer ;
     });
     element.html(itemsHTML);
 };
 
 // Event listeners
-$('.js-shopping-list-form').submit(function(event) {
+$('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    addItem(state, $('.shopping-list-entry').val());
+    addItem(state, $('#shopping-list-entry').val());
     renderList(state, $('.shopping-list'));
 });
 
