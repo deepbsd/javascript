5e872d8c07ae0a4c9af68bb8c5a67351f9f1c9b9
David S. Jackson <deepbsd@yahoo.com>
2017-02-04 21:03:03 -0500

    worked more on QuizzApp app.js

---
+++ projects/tf-QuizzApp/app.js
@@ -0,0 +1,149 @@
+"use strict;"
+
+// ###########  Single-State Data Object ###############
+// #####################################################
+var questions = [
+	{ 
+	  id: 1,
+	  question: "What is the first rule of the four (Jeff Cooper) firearms safety rules?",
+	  option_a: "Never point a gun at something you don't want to shoot.",
+	  option_b: "Always run away from guns.",
+	  option_c: '"All guns are always loaded."',
+	  option_d: "Keep your finger off the trigger until you're ready to destroy.",
+	  option_e: "Call your lawyer before you handle any gun.",
+	  answer: "c",
+	  correctly_answered: false
+	},
+
+	{ 
+		id: 2,
+		question: "What is the second rule of the four (Jeff Cooper) firearms safety rules?",
+		option_a: "Be certain of your target.",
+		option_b: "Unload all guns first thing.",
+		option_c: "Keep your finger off the trigger until your sights are on target and you're ready to fire.",
+		option_d: "Follow the orders of the safety officer.",
+		option_e: "Never let the muzzle cover anything you don't intend to destroy.",
+		answer: "e",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 3,
+		question: "What is the third fule of the four (Jeff Cooper) firearms safety rules?",
+		option_a: "Keep your finger off the trigger until your sights are on target and you're ready to fire.",
+		option_b: "Be certain of your target and what's behind it and in front of it.",
+		option_c: "Be aware of local range rules and regulations.",
+		option_d: "Follow relevant laws, both federal and local.",
+		option_e: "Never let the muzzle cover anything you don't intend to destroy.",
+		answer: "a",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 4,
+		question: "What is the fourth rule of the four (Jeff Cooper) firearms safety rules?",
+		option_a: "Know where all pets and children are before you fire.",
+		option_b: "Ensure guns are loaded before firing.",
+		option_c: "Keep your finger off the trigger until your sights are on target and you're ready to fire.",
+		option_d: "Be certain of your target and what's behind it and in front of it.",
+		option_e: "Be certain what will stop your bullet before you fire it.",
+		answer: "d",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 5,
+		question: "When holstering your handgun, where should your trigger finger be?",
+		option_a: "Inside the trigger guard.",
+		option_b: "Indexed and outside the trigger guard.",
+		option_c: "On the front of the trigger guard.",
+		option_d: "Curled inward toward the palm.",
+		option_e: "Inside your ear canal.",
+		answer: "b",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 6,
+		question: "When reholstering your handgun, how fast should you go?",
+		option_a: "Go as fast as you can.",
+		option_b: "Best to keep your gun out; wolves travel in packs!",
+		option_c: "There's no need to reholster quickly, take your time",
+		option_d: "You get bonus points for reholstering quicker than everyone else.",
+		option_e: "Just lay the gun down on the ground.",
+		answer: "c",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 7,
+		question: "Someone hands you a firearm in a gun store.  What should you do first?",
+		option_a: "Handling the gun safely, ensure the gun is unloaded and clear.",
+		option_b: "Lock the slide to the rear or open the bolt.",
+		option_c: "Drop the magazine, if there is one.",
+		option_d: "Open the cylinder and press the extractor rod.",
+		option_e: 'Ask that person, "Is it loaded?"',
+		answer: "a",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 8,
+		question: "You're clearing a semi-auto handgun.  What are the first two steps?",
+		option_a: "Rack the slide once and drop the magazine.",
+		option_b: "Drop the magazine and lock the slide to the rear.",
+		option_c: "Tap the magazine and lock the slide to the rear.",
+		option_d: "Drop the magazine, rack the slide, and re-insert the magazine.",
+		option_e: "Remove the magazine and look down the muzzle to see if there's a bullet in the chamber.",
+		answer: "b",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 9,
+		question: "You have a loaded double action revolver with the hammer cocked.  How can you make the gun safe?",
+		option_a: "Put your off-side thumb infront of the hammer and press the trigger, then unload as usual.",
+		option_b: "Put your strong-side thumb on the hammer and press the trigger to lower it gently onto the loaded chamber, then unload as usual.",
+		option_c: "Just leave the hammber cocked back and unload as usual.",
+		option_d: "Go ahead and press the trigger, the transfer bar will prevent ignition.  Then unload as usual.",
+		option_e: "Never unload a double action revolver; it's already safe.",
+		answer: "a",
+		correctly_answered: false
+	},
+
+	{ 
+		id: 10,
+		question: "Your 12-year-old son's friend shows him his father's gun.  What should your son do?",
+		option_a: "Call the police.",
+		option_b: "Call you at work.",
+		option_c: "Take the gun and demonstrate correct gun handling with the gun.",
+		option_d: "Explain the Jeff Cooper safety rules to his friend and show him this online quiz.",
+		option_e: "Politely ask his friend to not handle any guns without an adult present and leave immediately if the friend doesn't comply.",
+		answer: "e",
+		correctly_answered: false
+	}
+]
+
+
+// #############  State Modification Functions #######################
+// ###################################################################
+
+
+
+
+
+
+// ##############  Render Functions  ###################################
+// #####################################################################
+
+
+
+
+
+// ##############  Event Listeners  #####################################
+// ######################################################################
+
+
+
+
+
